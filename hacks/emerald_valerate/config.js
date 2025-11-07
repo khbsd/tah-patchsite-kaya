4ab56f647c853a554769dfef8d3504c75200615b
khbsd <khb.dev@gmail.com>
2025-11-06 21:44:23 -0600

    update: initial commit

---
+++ hacks/emerald_valerate/config.js
@@ -0,0 +1,11 @@
+window.TARP_CONFIG = {
+  title: "emerald valerate",
+  base: "Pokémon Emerald (USA, Europe)",
+
+  externalLinks: {
+    discord: "",
+    github: "https://github.com/khbsd/pokeemerald-kaya",
+    pokécommunity: "",
+    reddit: ""
+  }
+};
