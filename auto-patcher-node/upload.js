a96880e5510c91bcb31ba914a0aed4aa65679c96
khbsd <khb.dev@gmail.com>
2025-11-21 15:50:58 -0600

    update: better path checking

--- auto-patcher-node/upload.js
+++ auto-patcher-node/upload.js
@@ -1,36 +1,23 @@
 import { FilenSDK } from "@filen/sdk"
 import os from "os"
 import path from "path"
-import fs from "fs"
+import { statSync } from "fs"
 
 const filen = new FilenSDK({
 	metadataCache: true, // Cache decrypted metadata in memory. Recommended.
 	tmpPath: path.join(os.tmpdir(), "filen-sdk") // Temporary local path used to store metadata and chunks. Only available in Node.JS.
 })
 
-async function upload_file() {
-	let srcPath = process.argv[2];
-	let fileName = path.basename(srcPath);
-	let destFolderName = process.argv[3];
 
-	console.log(srcPath, fileName, destFolderName);
-
-	let isFile = await fs.stat(srcPath).isFile();
-	if (!isFile) {
-		return new Promise((resolve) => {
-			resolve();
-			console.log("source file not found, aborting");
-	});
-	}
-
+async function uploadFile(src, dest, fileName) {
 	try {
 		await filen.login({
 			email: process.env.USERNAME,
 			password: process.env.PASSWORD,
 		});
 		await filen.fs().upload({
-			path: path.join(destFolderName, fileName),
-			source: srcPath,
+			path: path.join(dest, fileName),
+			source: src,
 		}).then(() => console.log("file uploaded"));
 	} catch (err) { console.log ("error: ", err) }
 
@@ -41,4 +28,15 @@ async function upload_file() {
 		console.log("done");
 	});
 }
-upload_file();
+
+function handleArgs() {
+	let srcPath = process.argv[2];
+	let fileName = path.basename(srcPath);
+	let destFolderName = process.argv[3] ? process.argv[3] : "/romhacking-public";
+
+	if (statSync(srcPath).isFile())
+		uploadFile(srcPath, destFolderName, fileName);
+}
+
+handleArgs();
+
