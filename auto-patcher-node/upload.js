457208c76ee563e12348d9eb5db5c1e62ed2e6f7
khbsd <khb.dev@gmail.com>
2025-11-21 13:22:31 -0600

    fix: dont upload/overwrite non-existant files

--- auto-patcher-node/upload.js
+++ auto-patcher-node/upload.js
@@ -1,6 +1,7 @@
 import { FilenSDK } from "@filen/sdk"
 import os from "os"
 import path from "path"
+import fs from "fs"
 
 const filen = new FilenSDK({
 	metadataCache: true, // Cache decrypted metadata in memory. Recommended.
@@ -11,8 +12,17 @@ async function upload_file() {
 	let srcPath = process.argv[2];
 	let fileName = path.basename(srcPath);
 	let destFolderName = process.argv[3];
-	
+
 	console.log(srcPath, fileName, destFolderName);
+
+	let isFile = await fs.stat(srcPath).isFile();
+	if (!isFile) {
+		return new Promise((resolve) => {
+			filen.logout();
+			resolve();
+			console.log("source file not found, aborting");
+	});
+	}
 
 	try {
 		await filen.login({
