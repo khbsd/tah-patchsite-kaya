a07b318aabd4e666b023d82a703282c6b795b23e
khbsd <khb.dev@gmail.com>
2025-11-21 02:03:30 -0600

    connectToSocket fucked me

--- auto-patcher-node/upload.js
+++ auto-patcher-node/upload.js
@@ -2,16 +2,13 @@ import { FilenSDK } from "@filen/sdk"
 import os from "os"
 import path from "path"
 
-/*const filen = new FilenSDK({
+const filen = new FilenSDK({
 	metadataCache: true, // Cache decrypted metadata in memory. Recommended.
-	connectToSocket: true, // Recommended if you are using the virtual FS class. Keeps the internal item tree up to date with remote changes.
 	tmpPath: path.join(os.tmpdir(), "filen-sdk") // Temporary local path used to store metadata and chunks. Only available in Node.JS.
-})*/
-
-var isDone = false;
+})
 
 async function upload_file() {
-	/*try {
+	try {
 		await filen.login({
 			email: process.env.USERNAME,
 			password: process.env.PASSWORD,
@@ -20,20 +17,13 @@ async function upload_file() {
 			path: "/test/test.txt",
 			source: "./auto-patcher-node/upload-src/test.txt"
 		}).then(() => console.log("file uploaded"));
+	} catch (err) { console.log ("error: ", err) }
 
-	} catch (err) {
-		console.log("error: ", err);
-	}
-
-	isDone = true;*/
-	// filen.logout();
+	filen.logout();
 
 	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve(1);
-		}, 1000);
+		resolve();
 		console.log("done");
 	});
 }
-
 upload_file();
